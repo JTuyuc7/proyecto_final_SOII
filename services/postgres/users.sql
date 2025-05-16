32b665afa134e111263623af42bb7a49eb773684
Jaime Tuyuc <jaime.tuyuc@telus.com>
2025-05-15 19:08:26 -0600

    fix: db table and fields

--- services/postgres/users.sql
+++ services/postgres/users.sql
@@ -1,9 +1,11 @@
 DO $$
 BEGIN
-  FOR i IN 1..10000 LOOP
-    INSERT INTO usuarios (nombre, correo)
+  FOR i IN 1..5000 LOOP
+    INSERT INTO usuarios (username, nombre, apellido, correo)
     VALUES (
       CONCAT('Usuario_', i),
+      CONCAT('Nombre_', i),
+      CONCAT('Apellido_', i),
       CONCAT('usuario_', i, '@ejemplo.com')
     );
   END LOOP;
